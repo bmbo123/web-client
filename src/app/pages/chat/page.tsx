2c6cee77b48b1dcc25babc9235408543e1a27659
bmbo123 <gregoryalan2005@gmail.com>
2024-04-02 23:31:45 -0500

    just figured out the next routing

---
+++ src/app/pages/chat/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat: React.FC = () => {
+  return (
+    <main className="default-background min-h-screen flex items-center justify-center bg-gray-50">
+      <div>Hello World</div>
+    </main>
+  );
+};
+
+export default () => <Chat />;
